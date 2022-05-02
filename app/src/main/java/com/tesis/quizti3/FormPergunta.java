0693dfd1ac7324b3cb8fd9a7fb450946eb117f5b
Ellvis <tecnologia@geodigital.tech>
2022-05-01 21:42:49 -0300

    Envio da ultima etapa do parte 1 da cadeira, completo

---
+++ app/src/main/java/com/tesis/quizti3/FormPergunta.java
@@ -0,0 +1,14 @@
+package com.tesis.quizti3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormPergunta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pergunta);
+    }
+}
---
+++ app/src/main/java/com/tesis/quizti3/ListaPerguntasQuiz.java
@@ -0,0 +1,14 @@
+package com.tesis.quizti3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaPerguntasQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_perguntas_quiz);
+    }
+}
