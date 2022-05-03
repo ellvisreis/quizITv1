34e02a04e30ceb44b5bf905688452bf667b85226
Ellvis <tecnologia@geodigital.tech>
2022-05-03 11:54:46 -0300

    Reenvio por garantia, versao final do modulo 1

---
+++ app/src/main/java/com/tesis/quizti3/ListaQuizAluno.java
@@ -0,0 +1,14 @@
+package com.tesis.quizti3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaQuizAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_quiz_aluno);
+    }
+}
